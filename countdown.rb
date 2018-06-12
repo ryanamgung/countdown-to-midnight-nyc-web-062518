817addc2932cd3530bcb584a6229edc68db88f84
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-12 19:15:25 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(number)
   while number > 0 do
     puts "#{number} SECONDS(S) !"
-    number -= number
+    number -= 1
     
   end
   "HAPPY NEW YEAR!"
@@ -12,7 +12,7 @@ end
 def countdown_with_sleep(number)
   while number > 0 do
     puts "#{number} SECONDS(S)!"
-    number -= number
+    number -= 1
     sleep(1)  
   end
   "HAPPY NEW YEAR!"
